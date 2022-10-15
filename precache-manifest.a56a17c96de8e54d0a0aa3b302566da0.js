f44bfce16e7ca8ea57ba9a8f924c1b77db75afea
codekarshadab <76836545+codekarshadab@users.noreply.github.com>
2022-10-15 21:22:44 +0530

    Add files via upload

---
+++ precache-manifest.a56a17c96de8e54d0a0aa3b302566da0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e440f06122abdd13c01",
+    "url": "/static/css/main.eefe1d50.chunk.css"
+  },
+  {
+    "revision": "6e440f06122abdd13c01",
+    "url": "/static/js/main.7bee9de5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "74275cf8e748fb78d575",
+    "url": "/static/js/2.93261fc4.chunk.js"
+  },
+  {
+    "revision": "783e93c1ef2597e32a9db482cd8028aa",
+    "url": "/index.html"
+  }
+];
